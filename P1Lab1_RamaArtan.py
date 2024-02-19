a22f020d591cc727f858448ab4a27523612dd287
artanrama <160444643+artanrama@users.noreply.github.com>
2024-02-18 21:01:06 -0500

    Add files via upload
    First Submission ;)

---
+++ P1Lab1_RamaArtan.py
@@ -0,0 +1,13 @@
+#Artan Rama
+
+name=input('Artan Rama')
+
+#18 Feb 2024
+
+#P1Lab1
+
+#This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+print('Hey, name,
+      '\Welcome to zyBooks!')
+
