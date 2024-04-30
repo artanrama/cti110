f8eaec18db715de5807b9519c5298b1cf542904a
artanrama <160444643+artanrama@users.noreply.github.com>
2024-04-30 18:44:58 -0400

    Add files via upload

---
+++ P2Lab1_RamaArtan.py
@@ -0,0 +1,33 @@
+#Artan Rama
+
+#Feb 25 2024
+
+#P2Lab1
+
+#Assignment tests student's knowledge of how to write code that performs mathematical calculations and displays information to users
+
+milespergallon= float(input()) 
+
+dollarspergallon= float(input())
+
+#print(milespergallon)
+
+#print(dollarspergallon)
+
+dollarspermiles= dollarspergallon* 1/milespergallon
+
+#print(dollarspermiles)
+
+miles20= 20* dollarspermiles
+
+miles75= 75* dollarspermiles
+
+miles500= 500*dollarspermiles
+
+print(f'{miles20:.2f} {miles75:.2f} {miles500:.2f}')
+
+
+
+
+
+
