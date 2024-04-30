7a56c06085fde832c3d1e7ed1f13666c2b9fa60f
artanrama <160444643+artanrama@users.noreply.github.com>
2024-04-30 18:44:04 -0400

    Add files via upload

---
+++ P1H1_Rama_Artan.py
@@ -0,0 +1,15 @@
+#Artan Rama
+
+#Feb 23 2024
+
+#P1HW1
+
+#Assignment tests student's knowledge of how to write code that collects information from user, processes information collected and display results to user.
+
+user_num = int(input('\'Enter integer:\n'))
+print("You entered:", user_num)
+print(user_num, "squared is",user_num*user_num)
+print("And",user_num,"cubed is",user_num*user_num*user_num,"!!")  
+user_num2= int(input("Enter another integer:\n"))
+print(user_num,"+",user_num2,"is", user_num+user_num2)
+print(user_num,"*",user_num2,"is", user_num+user_num2)
