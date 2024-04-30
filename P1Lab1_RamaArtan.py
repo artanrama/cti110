722a92eaea020cf9500efc8e7274814500ccd2ac
artanrama <160444643+artanrama@users.noreply.github.com>
2024-04-30 18:44:37 -0400

    Add files via upload

--- P1Lab1_RamaArtan.py
+++ P1Lab1_RamaArtan.py
@@ -1,13 +1,16 @@
 #Artan Rama
 
-name=input('Artan Rama')
-
 #18 Feb 2024
 
 #P1Lab1
 
 #This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
 
-print('Hey, name,
-      '\Welcome to zyBooks!')
+
+
+name = input("Enter your name: ")
+print("Hey",name)
+print("Welcome to Zybooks!")
+      
+
 
